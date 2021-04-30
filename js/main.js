a506fb0a4c227a3ca7fdf2302e2f0b35bdcdd825
Emily <itwassme@gmail.com>
2021-05-01 07:51:32 +0900

    adjusted layouts

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 $('textarea').autoResize();
 
-
 // menu toggle
-$('.menu-toggle').on('click',function() {
-  
-  $('.mobile-menu').toggleClass('open')
-  
-  $(this).text($(this).text() == 'Menu' ? 'Close' : 'Menu')
-   
-  return false
-   
-})
+$('.menu-toggle').on('click', function () {
+  $('.mobile-menu').toggleClass('open');
 
+  // $(this).text($(this).html() == 'Menu' ? 'Close' : 'Menu');
+
+  var src = $('#mobile-menu').attr('src');
+  var newsrc =
+    src == 'assets/burger.svg' ? 'assets/close.svg' : 'assets/burger.svg';
+  $('#mobile-menu').attr('src', newsrc);
+
+  return false;
+});
 
 //  slide animation
 
@@ -21,7 +22,8 @@ let pageNumber = 0;
 // content for the pages
 const pages = [
   {
-    heading: 'OKR 수립과<br> 정렬(alignment)',
+    heading:
+      '<span lang="en">OKR</span> 수립과<br> 정렬<span lang="en">(alignment)</span>',
     paragraph:
       '조직간, 개인간 OKR을 연결할 수 있고, 진척도를 한 눈에 조회하고 관리할 수 있습니다. 팀 OKR 미팅, 스크럼(Scrum) 미팅에 효과적입니다',
     img: `<video width="441" height="390" autoplay muted loop>
@@ -35,7 +37,8 @@ const pages = [
     img: '<img src="assets/screen_02.png" />'
   },
   {
-    heading: 'OKR 실행 관리<br> (프로젝트, 과제, 이니셔티브 등)',
+    heading:
+      '<span lang="en">OKR</span> 실행 관리<br> (프로젝트, 과제, 이니셔티브 등)',
     paragraph:
       '핵심결과(Key Results)를 위한 다양한 과제/이니셔티브를 설정하고 진행을 관리합니다. OKR 외에도 업무관리나 프로젝트 매니지먼트에 효과적입니다.',
     img: `<video width="441" height="300" autoplay muted loop>
@@ -43,15 +46,15 @@ const pages = [
           </video>`
   },
   {
-    heading: '기업들의 새로운<br> 성과관리 문화, CFR',
+    heading: '새로운 성과관리 문화,<br><span lang="en">CFR</span>',
     paragraph:
-      '리더와 팀원, 누구와도 언제든지 쉽고 자연스럽게 일대일 미팅해요!',
+      '리더와 팀원, 누구와도 언제든지<br> 쉽고 자연스럽게 일대일 미팅해요',
     img: '<img src="assets/screen_04.png" />'
   },
   {
-    heading: '상시 인정과<br> 격려(Recognition)',
+    heading: '상시 인정과<br> 격려<span lang="en">(Recognition)</span>',
     paragraph:
-      '헤이 땡스(Hey-thanks)와 헤이 코인(Hey-coin)을 통해 언제든지 동료를 칭찬하고 감사를 표할 수 있습니다. 다양한 보/포상에까지 활용할 수 있어, 활발한 인정과 소통 문화를 만드는데 매우 효과적입니다.',
+      '헤이 땡스(Hey-thanks)와 헤이 코인(Hey-coin)을 통해 언제든지 동료를 칭찬하고 감사를 표할 수 있습니다. 다양한 보/포상에까지 활용할 수 있어, 활발한 인정과 소통문화를 만드는데 매우 효과적입니다.',
     img: '<img src="assets/screen_05.png" />'
   }
 ];
@@ -113,11 +116,9 @@ prevArrow.addEventListener('click', function () {
   setTimeout(removeAnimation, 800);
 });
 
-
 // scroll to topa
-const arrowTop = document.querySelector("img.top-arrow")
+const arrowTop = document.querySelector('img.top-arrow');
 
 function scrollToTop() {
-  window.scrollTo({top: 0, behavior: 'smooth'});
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 }
-
